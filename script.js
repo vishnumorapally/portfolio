9e07aea5c592a0ccc72a2ada3eb07044b3978f65
vishnumorapally <vishnumorapally2004@gmail.com>
2024-08-26 11:51:41 +0530

    3.0 sorting done

--- script.js
+++ script.js
@@ -5,9 +5,72 @@ const tags = document.querySelectorAll(".tg")
 
 const createbtn = (name)=>{
     let btn = document.createElement("button")
-    console.log(name)
+    btn.innerHTML = name
+    btn.id = "new"
     menu.append(btn)
-    console.log(menu)
+
+}
+
+const web = ()=>{
+    let a = ["HTML","CSS","JavaScript","React JS"]
+    a.forEach((name)=>{
+        createbtn(name)
+    })
+}
+
+const program = ()=>{
+    let a = ["PYTHON","JAVA"]
+    a.forEach((name)=>{
+        createbtn(name)
+    })
+}
+
+const operating = ()=>{
+    let a = ["WINDOWS","LINUX"]
+    a.forEach((name)=>{
+        createbtn(name)
+    })
+}
+
+const tools = ()=>{
+    let a = ["GIT"]
+    a.forEach((name)=>{
+        createbtn(name)
+    })
+}
+
+const database = ()=>{
+    let a = ["MySQL","MongoDB"]
+    a.forEach((name)=>{
+        createbtn(name)
+    })
+}
+
+const removealltags = ()=>{
+    let rweb = document.querySelectorAll(".w")
+    rweb.forEach((r)=>{
+        menu.removeChild(r)
+    })
+    let rpro = document.querySelectorAll(".p")
+    rpro.forEach((r)=>{
+        menu.removeChild(r)
+    })
+    let rope = document.querySelectorAll(".o")
+    rope.forEach((r)=>{
+        menu.removeChild(r)
+    })
+    let rtoo = document.querySelectorAll(".t")
+    rtoo.forEach((r)=>{
+        menu.removeChild(r)
+    })
+    let rdat = document.querySelectorAll(".d")
+    rdat.forEach((r)=>{
+        menu.removeChild(r)
+    })
+    let rnew = document.querySelectorAll("#new")
+    rnew.forEach((r)=>{
+        menu.removeChild(r)
+    })
 }
 
 function removeactive(){
@@ -18,12 +81,37 @@ function removeactive(){
 tags.forEach((e)=>{
     e.addEventListener("click",()=>{
         let btn = e.getAttribute("id")
-        console.log(btn)
         removeactive()
+        removealltags()
         e.classList.add("active")
-       let name = "#"+btn
-       createbtn(btn)
+
+        if (btn === "web"){
+            web()
+        }
+        else if(btn ===  "program"){
+            program()
+        }
+        else if(btn === "operating"){
+            operating()
+        }
+        else if(btn === "tools"){
+            tools()
+        }
+        else if (btn === "database"){
+            database()
+        }
+        else if(btn === "all"){
+            web()
+            program()
+            operating()
+            tools()
+            database()
+        }
     })
 
 })
+
+
+
+
 
